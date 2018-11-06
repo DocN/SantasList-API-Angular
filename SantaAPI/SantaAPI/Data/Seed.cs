6bb0796b34a699b3f4ad4a7e874c81ec74483e29
DrN <031247ryan@gmail.com>
2018-11-06 10:59:49 -0800

    added dummy data

---
+++ SantaAPI/SantaAPI/Data/Seed.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SantaAPI.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SantaAPI.Data
+{
+    public class Seed {
+        public static async Task Initialize(ApplicationDBContext context,
+                                RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
+        {
+            String adminId1 = "";
+            String adminId2 = "";
+
+            string role1 = "Admin";
+            string desc1 = "This is the administrator role";
+
+            string role2 = "Child";
+            string desc2 = "This is the members role";
+            context.Database.EnsureCreated();
+            if (await roleManager.FindByNameAsync(role1) == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole(role1));
+            }
+            if (await roleManager.FindByNameAsync(role2) == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole(role2));
+            }
+
+            if (await userManager.FindByEmailAsync("santa@np.com") == null)
+            {
+                IdentityUser newUser = new IdentityUser();
+                newUser.Email = "santa@np.com";
+                newUser.UserName = "santa";
+
+                var result = await userManager.CreateAsync(newUser, "P@$$w0rd");
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(newUser, "Admin");
+                }
+            }
+
+            if (await userManager.FindByEmailAsync("tim@np.com") == null)
+            {
+                IdentityUser newUser = new IdentityUser();
+                newUser.Email = "tim@np.com";
+                newUser.UserName = "tim";
+                var result = await userManager.CreateAsync(newUser, "P@$$w0rd");
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(newUser, "Child");
+                }
+            }
+
+        }
+    }
+}
--- SantaAPI/SantaAPI/Startup.cs
+++ SantaAPI/SantaAPI/Startup.cs
@@ -72,7 +72,7 @@ namespace SantaAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDBContext context, RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
         {
             if (env.IsDevelopment())
             {
@@ -86,6 +86,8 @@ namespace SantaAPI
             app.UseHttpsRedirection();
             app.UseAuthentication();
             app.UseMvc();
+
+            Seed.Initialize(context, roleManager, userManager).Wait();// seed here
         }
     }
 }
