9cc0511ab45868a754814d5213c15f4ec3f53f01
DrN <031247ryan@gmail.com>
2018-11-06 11:19:45 -0800

    started angular frontend

---
+++ SantaFrontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+//bootstrap
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
